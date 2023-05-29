0cf9f43aaf206a905ebb581ef6bca6e8bd234ecc
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-05-29 23:10:45 +0700

    init commit, chapter 2 parse identifier

---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import "testing"
+import "example/golang/token"
+
+func TestString(t *testing.T) {
+	input := "let myVar = anotherVar;"
+
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	if program.String() != input {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
---
+++ token/token.go
@@ -0,0 +1,60 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const ILLEGAL = "ILLEGAL"
+const EOF = "EOF"
+
+const IDENT = "IDENT"
+const INT = "INT"
+
+const ASSIGN = "="
+const PLUS = "+"
+const MINUS = "-"
+const BANG = "!"
+const ASTERISK = "*"
+const SLASH = "/"
+
+const LT = "<"
+const GT = ">"
+
+const EQUAL = "=="
+const NOT_EQUAL = "!="
+
+const COMMA = ","
+const SEMICOLON = ";"
+
+const LPAREN = "("
+const RPAREN = ")"
+const LBRACE = "{"
+const RBRACE = "}"
+
+const FUNCTION = "FUNCTION"
+const LET = "LET"
+const TRUE = "TRUE"
+const FALSE = "FALSE"
+const IF = "IF"
+const ELSE = "ELSE"
+const RETURN = "RETURN"
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+	"true": TRUE,
+	"false": FALSE,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
