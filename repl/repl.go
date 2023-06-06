9ee4e93d1799a1bba5e732a7314549eeecca029c
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-06 23:02:08 +0700

    update repl

--- repl/repl.go
+++ repl/repl.go
@@ -4,13 +4,14 @@ import "bufio"
 import "fmt"
 import "io"
 import "mongkee/lexer"
-import "mongkee/token"
+import "mongkee/parser"
+import "mongkee/evaluator"
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
-	
+
 	for {
 		fmt.Fprintf(out, PROMPT)
 		scanned := scanner.Scan()
@@ -20,10 +21,30 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-		l := lexer.New(line)
-
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		if line == "exit" {
+			break
 		}
+
+		l := lexer.New(line)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
+		}
+
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "parser errors:\n")
+	for _, error := range errors {
+		io.WriteString(out, "\t"+error+"\n")
 	}
 }
