e8e676abad237a6e562a4e91ff2efeb678c2b469
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-06 22:09:04 +0700

    evaluate integer

---
+++ object/object.go
@@ -0,0 +1,18 @@
+package object
+
+import "fmt"
+
+const INTEGER_OBJ = "INTEGER"
+
+type ObjectType string
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
