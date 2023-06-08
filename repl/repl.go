4781a3f88cdbe0b95a515cbea8a17c0e87868d5a
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-08 21:31:13 +0700

    evaluate let statement

--- repl/repl.go
+++ repl/repl.go
@@ -5,12 +5,14 @@ import "fmt"
 import "io"
 import "mongkee/lexer"
 import "mongkee/parser"
+import "mongkee/object"
 import "mongkee/evaluator"
 
 const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprintf(out, PROMPT)
@@ -34,7 +36,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
