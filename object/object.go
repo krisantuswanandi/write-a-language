38d26f6874ee50b0550ada9f40ca5ecc828c1665
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-10 17:14:45 +0700

    evaluate array literal

--- object/object.go
+++ object/object.go
@@ -13,6 +13,7 @@ const ERROR_OBJ = "ERROR"
 const FUNCTION_OBJ = "FUNCTION"
 const STRING_OBJ = "STRING"
 const BUILTIN_OBJ = "BUILTIN"
+const ARRAY_OBJ = "ARRAY"
 
 type ObjectType string
 
@@ -94,3 +95,23 @@ type Builtin struct {
 
 func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Builtin) Inspect() string  { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (a *Array) Type() ObjectType { return ARRAY_OBJ }
+func (a *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, e := range a.Elements {
+		elements = append(elements, e.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+	
+	return out.String()
+}
