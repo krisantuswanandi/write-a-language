37b094ea425dc1b086b1e5d374c056974da9fc8b
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-14 09:55:20 +0700

    add repl

---
+++ typescript/src/repl/repl.ts
@@ -0,0 +1,17 @@
+import { TOKEN } from "../token/token.ts"
+import { Lexer } from "../lexer/lexer.ts"
+
+console.write(">> ");
+for await (const line of console) {
+	if (line === "exit") break
+
+	const lexer = new Lexer(line)
+	while (true) {
+		const token = lexer.nextToken()
+		console.log(`${token.type}("${token.literal}`)
+
+		if (token.type === TOKEN.EOF) break
+	}
+
+	console.write(">> ");
+}
