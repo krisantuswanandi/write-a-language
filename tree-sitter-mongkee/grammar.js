c6c2780757ed1e8caa125a571b38b1bec5870bda
Krisantus Wanandi <krisantus.wanandi@gmail.com>
2023-06-26 00:25:32 +0700

    tree sitter parse let statement

--- tree-sitter-mongkee/grammar.js
+++ tree-sitter-mongkee/grammar.js
@@ -1,6 +1,16 @@
 module.exports = grammar({
   name: "mongkee",
   rules: {
-    source_file: $ => 'hello'
+    program: $ => repeat($.statement),
+    statement: $ => choice($.let_statement),
+    let_statement: $ => seq(
+      'let',
+      $.identifier,
+      '=',
+      $.expression,
+    ),
+    expression: $ => $.integer_literal,
+    identifier: () => /[a-zA-Z_]+/,
+    integer_literal: () => /\d+/,
   }
 });
